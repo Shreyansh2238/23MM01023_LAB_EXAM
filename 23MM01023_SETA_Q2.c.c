ca55129eca67809d90e3f88ef096c06fa2d692d6
Shreyansh2238 <148929367+Shreyansh2238@users.noreply.github.com>
2024-02-16 15:50:10 +0530

    Add files via upload

---
+++ 23MM01023_SETA_Q2.c.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main(){
+    int a,b,i,j,sum=0;
+    printf("Input lower limit: ");
+    scanf("%d",&a);
+    printf("Input upper limit: ");
+    scanf("%d",&b);
+    for(i=a;i<=b;i++)
+    {
+        sum=0;
+        for(j=1;j<i;j++)
+        {
+            if(i%j==0)
+            {
+                sum+=j;
+            }
+        }
+        
+        if(sum==i)
+        {
+            printf("%d is a perfect number.\n",i);
+        }
+    }
+    
+    return 0;
+    
+}
